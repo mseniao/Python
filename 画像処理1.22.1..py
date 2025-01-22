4686b4ebcff946a9bc9ddd6796bd667c2b80326d
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-22 17:33:56 +0900

    1.22

--- 画像処理1.22.1..py
+++ 画像処理1.22.1..py
@@ -7,21 +7,22 @@ while(True):
     if vtr is None:
         print("映像を取得できません")
         break
-    ref = cv2.resize(vtr, dsize=None, fx=0.3, fy=0.3) #全体が映るようにリサイズ
+    ref = cv2.resize(vtr, dsize=None, fx=0.7, fy=0.7) #全体が映るようにリサイズ
     grf = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
     neg = cv2.bitwise_not(grf)  #黒線を強調するためにネガポジ反転
-    lines = cv2.HoughLinesP(neg, rho=1, theta=np.pi/360, threshold=100, minLineLength=300, maxLineGap=5) #ライン検出
+    lines = cv2.HoughLinesP(neg, rho=1, theta=np.pi/360, threshold=100, minLineLength=100, maxLineGap=5) #ライン検出
 
-    #for line in lines:
-    #    x1, y1, x2, y2 = line[0]
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
 
         # 赤線を引く
-    #    cv2.line(ref, (x1,y1), (x2,y2), (0,0,255), 3)
-    #    cv2.imwrite('linewriter.jpg', ref)
+        cv2.line(ref, (x1,y1), (x2,y2), (0,0,255), 3)
+        cv2.imwrite('linewriter.jpg', ref)
+    n = cv2.imread("linewriter.jpg")
 
-    cv2.imshow('f',neg)
+    cv2.imshow('f',n)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-vtr.release()
+
 cv2.destroyAllWindows()
 
