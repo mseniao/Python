9dc25a512c326581854106e241fca9d6dc08bba8
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-22 14:43:58 +0900

    1.22

--- 映像処理1.22.py
+++ 映像処理1.22.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 vtr = cv2.VideoCapture("IMG_5578.MOV")
 while(True):
@@ -9,7 +10,9 @@ while(True):
     ref = cv2.resize(frame, dsize=None, fx=0.3, fy=0.3) #全体が映るようにリサイズ
     grf = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
     neg = cv2.bitwise_not(grf)  #黒線を強調するためにネガポジ反転
-    
+    lines = cv2.HoughLinesP(neg, rho=1, theta=np.pi/360, threshold=80, minLineLength=400, maxLineGap=5)
+    print(lines)
+
     cv2.imshow('f',neg)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
