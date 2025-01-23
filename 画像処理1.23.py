620e088cef7a9611d2bfba8d36d3b15530a37442
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-23 14:00:12 +0900

    1.23

--- 画像処理1.23.py
+++ 画像処理1.23.py
@@ -9,7 +9,7 @@ while(True):
         break
     ref = cv2.resize(vtr, dsize=None, fx=0.5, fy=0.5) #全体が映るようにリサイズ
     grf = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
-    can = cv2.Canny(grf,80.0,200.0)  #canny処理
+    can = cv2.Canny(grf,40.0,200.0)  #canny処理
     lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=50, minLineLength=100, maxLineGap=5) #ライン検出
 
     for line in lines:
@@ -19,8 +19,8 @@ while(True):
         cv2.line(ref, (x1,y1), (x2,y2), (0,0,255), 3)
         cv2.imwrite('linewriter.jpg', ref)
    
-    cv2.imshow('f',ref)
-    #cv2.imshow('d',can)
+    cv2.imshow('f',can)
+    cv2.imshow('g',ref)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
