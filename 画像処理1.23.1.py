36e1b2ec4929438803d621993f5c8b89cec1a6ee
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-23 14:58:36 +0900

    1.23からHoughLinesP内部を調整

--- 画像処理1.23.1.py
+++ 画像処理1.23.1.py
@@ -10,14 +10,14 @@ while(True):
     ref = cv2.resize(vtr, dsize=None, fx=0.5, fy=0.5) #全体が映るようにリサイズ
     grf = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
     can = cv2.Canny(grf,40.0,200.0)  #canny処理
-    lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=50, minLineLength=100, maxLineGap=5) #ライン検出
+    lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=30, minLineLength=25, maxLineGap=5) #ライン検出
 
     for line in lines:
         x1, y1, x2, y2 = line[0]
 
         # 赤線を引く
         cv2.line(ref, (x1,y1), (x2,y2), (0,0,255), 3)
-        cv2.imwrite('linewriter.jpg', ref)
+        #cv2.imwrite('linewriter.jpg', ref)
    
     cv2.imshow('f',can)
     cv2.imshow('g',ref)
