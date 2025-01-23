8005dff2b60e35b7dd0903d00bc2700c106d642d
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-23 15:01:25 +0900

    1.23.1から輝度平滑化を実施

--- 画像処理1.23.2.py
+++ 画像処理1.23.2.py
@@ -9,7 +9,8 @@ while(True):
         break
     ref = cv2.resize(vtr, dsize=None, fx=0.5, fy=0.5) #全体が映るようにリサイズ
     grf = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
-    can = cv2.Canny(grf,40.0,200.0)  #canny処理
+    equ = cv2.equalizehist(grf)
+    can = cv2.Canny(equ,40.0,200.0)  #canny処理
     lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=30, minLineLength=25, maxLineGap=5) #ライン検出
 
     for line in lines:
@@ -19,8 +20,8 @@ while(True):
         cv2.line(ref, (x1,y1), (x2,y2), (0,0,255), 3)
         #cv2.imwrite('linewriter.jpg', ref)
    
-    cv2.imshow('f',can)
-    cv2.imshow('g',ref)
+    cv2.imshow('g',grf)
+    cv2.imshow('e',equ)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
