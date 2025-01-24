a0bc2eb6d56702a3c392cb2fb2dad4ec84db33a1
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-24 17:02:43 +0900

    1.24.1のwhile(true)ループ削除+他いろいろ改善

--- 画像処理1.24.2.py
+++ 画像処理1.24.2.py
@@ -9,7 +9,7 @@ if vtr is None:
    
 grf = cv2.cvtColor(vtr, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
 can = cv2.Canny(grf,40.0,200.0)  #canny処理
-lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=100, minLineLength=300, maxLineGap=50) #ライン検出
+lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=100, minLineLength=80, maxLineGap=50) #ライン検出
 
 for line in lines:
     x1, y1, x2, y2 = line[0]
@@ -19,7 +19,7 @@ for line in lines:
 
 ref = cv2.resize(vtr, dsize=None, fx=0.5, fy=0.5) #全体が映るようにリサイズ
 cv2.imwrite('linewriter.jpg', ref)
-#cv2.imshow('f',can)
+cv2.imshow('f',can)
 cv2.imshow('g',ref)
 cv2.waitKey(0)
 
