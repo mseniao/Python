cca051574e26221dbb74bc8f5a386e91cab50c0c
mseniao <152139264+mseniao@users.noreply.github.com>
2025-01-24 13:54:47 +0900

    1.23.1+ダメージ補修inpaint

--- 画像処理1.23.4.py
+++ 画像処理1.23.4.py
@@ -10,7 +10,7 @@ while(True):
     ref = cv2.resize(vtr, dsize=None, fx=0.5, fy=0.5) #全体が映るようにリサイズ
     grf = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY) #判別をしやすくするためにグレースケール化
     ret, msk = cv2.threshold(grf,230,255,cv2.THRESH_BINARY)
-    rep = cv2.inpaint(grf,msk,1,cv2.INPAINT_TELEA)
+    rep = cv2.inpaint(ref,msk,1,cv2.INPAINT_NS)
     can = cv2.Canny(rep,40.0,200.0)  #canny処理
     lines = cv2.HoughLinesP(can, rho=1, theta=np.pi/360, threshold=30, minLineLength=25, maxLineGap=5) #ライン検出
 
@@ -19,7 +19,7 @@ while(True):
 
         # 赤線を引く
         cv2.line(ref, (x1,y1), (x2,y2), (0,0,255), 3)
-        #cv2.imwrite('linewriter.jpg', ref)
+        cv2.imwrite('linewriter.jpg', ref)
    
     cv2.imshow('f',can)
     cv2.imshow('g',rep)
